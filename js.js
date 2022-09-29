02976455bb0241d754f2a23774243c5b09906cbf
east-west-meni <114690420+east-west-meni@users.noreply.github.com>
2022-09-29 21:46:15 +0200

    V2
    *Javascript*

--- js.js
+++ js.js
@@ -1,1 +1,26 @@
-console.log("connected")
+let zastor = document.getElementById("poklopac");
+setTimeout(()=>{
+    zastor.style.transform ="translateY(-3000px)"}
+    ,1000)
+    
+
+let dropdown = document.getElementById("dropdown")
+function dropdown_toggle(e) {
+    if (e) {
+        dropdown.classList.add("show")
+    }
+    else {
+        dropdown.classList.remove("show")
+    }
+}
+
+
+// nav skrollanje
+function skrolaj(ime){
+    let el = document.getElementById(ime);
+    el.scrollIntoView({
+        behavior: "smooth",
+        block: "center"
+    });
+}
+
